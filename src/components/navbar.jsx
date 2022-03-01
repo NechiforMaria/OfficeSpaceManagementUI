91d647c6f424a687909f1215f94859b73397bb6c
Daniel Ioan Flutur <ioan.flutur@student.usv.ro>
2022-03-01 19:49:25 +0200

    Edit company name

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,7 +7,7 @@ class NavBar extends Component {
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <div className="container-fluid">
             <a className="navbar-brand" href="#">
-              Company name
+              No More Bugs
             </a>
             <button
               className="navbar-toggler"
