6378ea5004427c4fbca46a348acf30ffa13e42b2
NechiforMaria <bianca.nechifor@student.usv.ro>
2022-03-09 23:42:03 +0200

    I added components to the request page

--- src/Administrator/Request.jsx
+++ src/Administrator/Request.jsx
@@ -1,11 +1,32 @@
 import React, { Component } from "react";
+import Slider from "../components/Slider";
+import "./Request.css";
 
 export class Request extends Component {
   render() {
     return (
-      <div>
-        <h3>This is Request page</h3>
-      </div>
+      <React.Fragment>
+        <br></br>
+        <h2 className=" m-2">Request to work remote</h2>
+        <br></br>
+        <label className=" m-2">
+          Percentage of remote work time from a month *
+        </label>
+        <Slider />
+        <div>
+          <label className=" m-2">
+            Reason or which they are making this request *
+          </label>
+        </div>
+        <div>
+          <input className="comment m-2"></input>
+        </div>
+
+        <div>
+          <label className=" m-2">All fields with * must be completed</label>
+        </div>
+        <button className="btn btn-primary m-2">Request</button>
+      </React.Fragment>
     );
   }
 }
