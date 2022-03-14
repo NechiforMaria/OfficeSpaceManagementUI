c7e8cbbf5a6603d5d63750d3fc107eb3b12335bc
[ioana.iurcu] <[ioana.iurcu@student.usv.ro]>
2022-03-14 10:06:16 +0200

    Modified component style in navbarUser

--- src/components/navbar/navbarUser.jsx
+++ src/components/navbar/navbarUser.jsx
@@ -12,40 +12,54 @@ class UserNavBar extends React.Component {
               <img className="logo" src={logo} alt="No More Bugs" />
             </div>
           </div>
-          <div style={mystyledreapta} className="collapse navbar-collapse " id="navbarNavDropdown">
-            <ul className="navbar-nav ">
-              <li className="nav-item dropdown me-2 ">
-                <span
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbarDropdownMenuLink"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Status
-                </span>
-                <ul className="dropdown-menu">
-                  <li className="dropdown-item color: black">
-                    <NavLink className="nav-link" to="/userStatus">
-                      User Status
-                    </NavLink>
-                  </li>
-                  <li className="dropdown-item color: black">Office Status</li>
-                </ul>
-              </li>
+          <div style={mystyleflex} className="justify-content-end">
+            <div
+              style={mystyledreapta}
+              className="collapse navbar-collapse "
+              id="navbarNavDropdown"
+            >
+              <ul className="navbar-nav ">
+                <li className="nav-item dropdown me-2 ">
+                  <span
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    id="navbarDropdownMenuLink"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Status
+                  </span>
+                  <ul
+                    className="dropdown-menu"
+                    style={{backgroundColor: "black"}}
+                    aria-labelledby="navbarDropdownMenuLink"
+                  >
+                    <li className="dropdown-item color: black">
+                      <NavLink className="nav-link" to="/userStatus">
+                        User Status
+                      </NavLink>
+                    </li>
+                    <li className="dropdown-item color: black">
+                      <NavLink className="nav-link" to="/officeStatus">
+                        Office Status
+                      </NavLink>
+                    </li>
+                  </ul>
+                </li>
 
-              <li>
-                <NavLink to="/DeskAssignment" className="nav-link">
-                  Desk Assignment
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/Request" className="nav-link">
-                  Request
-                </NavLink>
-              </li>
-            </ul>
+                <li>
+                  <NavLink className="nav-link" to="/DeskAssignment">
+                    Desk Assignment
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink className="nav-link" to="/Request">
+                    Request
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
           </div>
         </nav>
       </nav>
@@ -54,5 +68,8 @@ class UserNavBar extends React.Component {
 }
 const mystyledreapta = {
   float: "right",
+};
+const mystyleflex = {
+  display: "flex",
 };
 export default UserNavBar;
