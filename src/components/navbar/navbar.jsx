c4fce09a67d69274c6bac915bb679524c855262d
[ioana.iurcu] <[ioana.iurcu@student.usv.ro]>
2022-03-15 00:48:25 +0200

    Update navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import {NavLink} from "react-router-dom";
 import "./navbar.css";
-import logo from "./logo_transparent.png";
+import logo from "./Logo.jpg";
 class NavBar extends Component {
   state = {
     persons: [],
