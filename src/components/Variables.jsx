b6686cf429b5de6b832f9c425784578dfd814d80
NechiforMaria <bianca.nechifor@student.usv.ro>
2022-03-16 00:25:00 +0200

    I changed the API host

--- src/components/Variables.jsx
+++ src/components/Variables.jsx
@@ -1,4 +1,3 @@
 export const variables = {
-  API_URL:
-    "https://nmbofficemanagementapi.azurewebsites.net/?fbclid=IwAR0bnNrNerQAa5esnRhVDDj_HbU9rafF3LHvSnsVD6kM0Q8aBuiFs8qlJzs/api/",
+  API_URL: "https://nmbofficemanagementapi.azurewebsites.net/api/",
 };
