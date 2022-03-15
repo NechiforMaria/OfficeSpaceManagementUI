f8d9dc127dadf335dd86e9b6919ba4df729a0cc0
[ioana.iurcu] <[ioana.iurcu@student.usv.ro]>
2022-03-15 21:03:15 +0200

    Changed style for Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import "./Footer.css";
-export default class Footer extends React.Component {
+
+export class Footer extends React.Component {
   render() {
     return (
-      <div className="footer">
-        <p display="inline">Number: 0372 092 201</p>
-        <p>Address: Strada Universitatii 15-17</p>
+      <footer className="footer">
+        <span>
+          <p display="inline">Number: 0372 092 201</p>
+          <p>Address: Strada Universitatii 15-17</p>
+        </span>
+
         <p>&copy; All rights reserved. Website developed by No More Bugs team.</p>
-      </div>
+      </footer>
     );
   }
 }
+export default Footer;
