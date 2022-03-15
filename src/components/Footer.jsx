89a43898174ba65c841a0a6af314b3c1be02be1a
[ioana.iurcu] <[ioana.iurcu@student.usv.ro]>
2022-03-15 20:00:28 +0200

    Added Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class Footer extends React.Component {
+  render() {
+    return (
+      <div className="footer">
+        <p display="inline">Number: 0372 092 201</p>
+        <p>Address: Strada Universitatii 15-17</p>
+        <p>&copy; All rights reserved. Website developed by No More Bugs team.</p>
+      </div>
+    );
+  }
+}
