152a1df31193dd8d0156ff00f26ab2ef6774f1b2
NechiforMaria <bianca.nechifor@student.usv.ro>
2022-03-16 00:11:56 +0200

    I added new directory

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Footer.css";
+
+export class Footer extends React.Component {
+  render() {
+    return (
+      <div className="footer">
+        <span>
+          <p display="inline">Number: 0372 092 201</p>
+          <p>Address: Strada Universitatii 15-17</p>
+        </span>
+        <p>
+          &copy; All rights reserved. Website developed by No More Bugs team.
+        </p>
+      </div>
+    );
+  }
+}
+export default Footer;
